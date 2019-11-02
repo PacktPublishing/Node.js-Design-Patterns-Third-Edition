4efa8926383025a20f4a1980eeeac40c7182156c
Luciano <lucianomammino@gmail.com>
2019-11-02 18:59:52 +0000

    Added callback iteration pattern

---
+++ 04-asynchronous-control-flow-patterns-with-callbacks/05-callback-iteration-pattern/README.md
@@ -0,0 +1,11 @@
+# 05-callback-iterator-pattern
+
+Simple example that demostrates the callback iteration pattern
+
+## Run
+
+Run:
+
+```bash
+node test.js
+```
---
+++ 04-asynchronous-control-flow-patterns-with-callbacks/05-callback-iteration-pattern/test.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const tasks = [
+  (cb) => {
+    console.log('Task 1')
+    setTimeout(cb, 1000)
+  },
+  (cb) => {
+    console.log('Task 2')
+    setTimeout(cb, 1000)
+  },
+  (cb) => {
+    console.log('Task 3')
+    setTimeout(cb, 1000)
+  }
+]
+
+function iterate (index) {
+  if (index === tasks.length) {
+    return finish()
+  }
+  const task = tasks[index]
+  task(() => iterate(index + 1))
+}
+
+function finish () {
+  // iteration completed
+  console.log('All tasks executed')
+}
+
+iterate(0)
