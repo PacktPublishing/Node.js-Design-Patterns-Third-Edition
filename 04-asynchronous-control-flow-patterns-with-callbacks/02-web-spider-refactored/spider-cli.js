575fe961722fe8063c22daf99713208a39412610
Luciano <lucianomammino@gmail.com>
2019-11-02 12:37:40 +0000

    Added spider refactored

---
+++ 04-asynchronous-control-flow-patterns-with-callbacks/02-web-spider-refactored/spider-cli.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import spider from './spider.js'
+
+spider(process.argv[2], (err, filename, downloaded) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+
+  if (downloaded) {
+    console.log(`Completed the download of "${filename}"`)
+  } else {
+    console.log(`"${filename}" was already downloaded`)
+  }
+})
---
+++ 04-asynchronous-control-flow-patterns-with-callbacks/02-web-spider-refactored/spider.js
@@ -0,0 +1,49 @@
+'use strict'
+
+import fs from 'fs'
+import path from 'path'
+import request from 'request'
+import mkdirp from 'mkdirp'
+import { urlToFilename } from './utils.js'
+
+function saveFile (filename, contents, cb) {
+  mkdirp(path.dirname(filename), err => {
+    if (err) {
+      return cb(err)
+    }
+    fs.writeFile(filename, contents, cb)
+  })
+}
+
+function download (url, filename, cb) {
+  console.log(`Downloading ${url}`)
+  request(url, (err, response, body) => {
+    if (err) {
+      return cb(err)
+    }
+    saveFile(filename, body, err => {
+      if (err) {
+        return cb(err)
+      }
+      console.log(`Downloaded and saved: ${url}`)
+      cb(null, body)
+    })
+  })
+}
+
+function spider (url, cb) {
+  const filename = urlToFilename(url)
+  fs.access(filename, err => {
+    if (!err || err.code !== 'ENOENT') {
+      return cb(null, filename, false)
+    }
+    download(url, filename, err => {
+      if (err) {
+        return cb(err)
+      }
+      cb(null, filename, true)
+    })
+  })
+}
+
+export default spider
