102c8e2a5e5db424d6625bb5bcfa7d99b2eb7d8d
Luciano <lucianomammino@gmail.com>
2019-11-03 10:12:50 +0000

    Added unlimited parallel execution

---
+++ 04-asynchronous-control-flow-patterns-with-callbacks/07-callback-unlimited-parallel-execution-pattern/README.md
@@ -0,0 +1,11 @@
+# 07-callback-unlimited-parallel-execution-pattern
+
+Simple example that demostrates the callback parallel execution pattern
+
+## Run
+
+Run:
+
+```bash
+node test.js
+```
---
+++ 04-asynchronous-control-flow-patterns-with-callbacks/07-callback-unlimited-parallel-execution-pattern/test.js
@@ -0,0 +1,32 @@
+'use strict'
+
+function makeSampleTask (name) {
+  return (cb) => {
+    console.log(`${name} started`)
+    setTimeout(() => {
+      console.log(`${name} completed`)
+      cb()
+    }, Math.random() * 2000)
+  }
+}
+
+const tasks = [
+  makeSampleTask('Task 1'),
+  makeSampleTask('Task 2'),
+  makeSampleTask('Task 3'),
+  makeSampleTask('Task 4')
+]
+
+let completed = 0
+tasks.forEach(task => {
+  task(() => {
+    if (++completed === tasks.length) {
+      finish()
+    }
+  })
+})
+
+function finish () {
+  // all the tasks completed
+  console.log('All tasks executed!')
+}
