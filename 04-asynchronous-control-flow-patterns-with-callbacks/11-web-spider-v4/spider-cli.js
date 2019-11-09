45cbcbdb1133abeac850d50f2545c815d37f4b58
Luciano <lucianomammino@gmail.com>
2019-11-09 11:40:29 +0000

    concurrency is a better word

--- 04-asynchronous-control-flow-patterns-with-callbacks/11-web-spider-v4/spider-cli.js
+++ 04-asynchronous-control-flow-patterns-with-callbacks/11-web-spider-v4/spider-cli.js
@@ -5,9 +5,9 @@ import spider from './spider.js'
 
 const url = process.argv[2]
 const nesting = Number.parseInt(process.argv[3]) || 1
-const parallel = Number.parseInt(process.argv[4]) || 2
+const concurrency = Number.parseInt(process.argv[4]) || 2
 
-const spiderQueue = new TaskQueue(parallel)
+const spiderQueue = new TaskQueue(concurrency)
 spiderQueue.on('error', console.error)
 spiderQueue.on('empty', () => console.log('Download complete'))
 
