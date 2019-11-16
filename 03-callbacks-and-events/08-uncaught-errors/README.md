605d5362f0b524c66671ca6585efa362fec794e2
Mario Casciaro <mariocasciaro@gmail.com>
2019-11-16 18:32:20 +0000

    Added 03/08-uncaught-exceptions

---
+++ 03-callbacks-and-events/08-uncaught-errors/README.md
@@ -0,0 +1,11 @@
+# 08-uncaught-errors
+
+This example demonstrates how uncaught errors will cause the application to exit. It also shows how to catch those errors just before the application exits.
+
+## Run
+
+To run the example launch:
+
+```bash
+node index.js
+```
---
+++ 03-callbacks-and-events/08-uncaught-errors/index.js
@@ -0,0 +1,26 @@
+import fs from 'fs'
+
+function readJSONThrows (filename, callback) {
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+      return callback(err)
+    }
+    callback(null, JSON.parse(data))
+  })
+}
+
+// The error is not propagated to the final callback nor is caught
+// by a try/catch statement
+try {
+  readJSONThrows('invalid_json.json', (err) => console.error(err))
+} catch (err) {
+  console.log('This will NOT catch the JSON parsing exception')
+}
+
+// Our last chance to intercept any uncaught error
+process.on('uncaughtException', (err) => {
+  console.error(`This will catch at last the JSON parsing exception: ${err.message}`)
+  // Terminates the application with 1 (error) as exit code.
+  // Without the following line, the application would continue
+  process.exit(1)
+})
