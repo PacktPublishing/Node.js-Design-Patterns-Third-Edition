1d7dc6d80c28501c6f173d623fd36f18b8bc27f9
Luciano <lucianomammino@gmail.com>
2019-12-13 11:05:38 +0000

    fixed eslint issues

---
+++ 02-the-module-system/04-cjs-named-exports/main.js
@@ -0,0 +1,3 @@
+const logger = require('./logger')
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
--- 04-asynchronous-control-flow-patterns-with-callbacks/10-queue-limited-parallel-execution-with-events/TaskQueue.js
+++ 04-asynchronous-control-flow-patterns-with-callbacks/10-queue-limited-parallel-execution-with-events/TaskQueue.js
@@ -1,5 +1,3 @@
-'use strict'
-
 import { EventEmitter } from 'events'
 
 class TaskQueue extends EventEmitter {
