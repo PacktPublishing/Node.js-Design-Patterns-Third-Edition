fb7d018bff41e58f57f9070ed0524708d2be2724
Luciano <lucianomammino@gmail.com>
2020-01-12 16:36:04 +0000

    Added crypto-gzip example

--- 06-coding-with-streams/03-crypto-gzip-server/crypto-gzip-receive.js
+++ 06-coding-with-streams/03-crypto-gzip-server/crypto-gzip-receive.js
@@ -2,12 +2,18 @@ import { createServer } from 'http'
 import { createWriteStream } from 'fs'
 import { createGunzip } from 'zlib'
 import { basename, join } from 'path'
+import { createDecipheriv, randomBytes } from 'crypto'
+
+const secret = randomBytes(24)
+console.log(`Generated secret: ${secret.toString('hex')}`)
 
 const server = createServer((req, res) => {
   const filename = basename(req.headers['x-filename'])
+  const iv = Buffer.from(req.headers['x-initialization-vector'], 'hex')
   const destFilename = join('received_files', filename)
   console.log(`File request received: ${filename}`)
   req
+    .pipe(createDecipheriv('aes192', secret, iv))
     .pipe(createGunzip())
     .pipe(createWriteStream(destFilename))
     .on('finish', () => {
--- 06-coding-with-streams/03-crypto-gzip-server/crypto-gzip-send.js
+++ 06-coding-with-streams/03-crypto-gzip-server/crypto-gzip-send.js
@@ -2,9 +2,13 @@ import { request } from 'http'
 import { createGzip } from 'zlib'
 import { createReadStream } from 'fs'
 import { basename } from 'path'
+import { createCipheriv, randomBytes } from 'crypto'
 
 const filename = process.argv[2]
 const serverHost = process.argv[3]
+const secret = Buffer.from(process.argv[4], 'hex')
+
+const iv = randomBytes(16)
 
 const httpRequestOptions = {
   hostname: serverHost,
@@ -14,7 +18,8 @@ const httpRequestOptions = {
   headers: {
     'Content-Type': 'application/octet-stream',
     'Content-Encoding': 'gzip',
-    'X-Filename': basename(filename)
+    'X-Filename': basename(filename),
+    'X-Initialization-Vector': iv.toString('hex')
   }
 }
 
@@ -24,6 +29,7 @@ const req = request(httpRequestOptions, (res) => {
 
 createReadStream(filename)
   .pipe(createGzip())
+  .pipe(createCipheriv('aes192', secret, iv))
   .pipe(req)
   .on('finish', () => {
     console.log('File successfully sent')
