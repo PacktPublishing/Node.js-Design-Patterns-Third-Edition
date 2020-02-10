cb1663601f00ef898c1280cc9469eacfb68d22a8
Luciano Mammino <lucianomammino@gmail.com>
2020-02-09 16:34:55 -0800

    Updated proxy reactive

--- 08-structural-design-patterns/06-proxy-reactive/create-observable.js
+++ 08-structural-design-patterns/06-proxy-reactive/create-observable.js
@@ -1,8 +1,11 @@
-export function createObservable (target, listener) {
+export function createObservable (target, observer) {
   const observable = new Proxy(target, {
     set (obj, prop, value) {
-      obj[prop] = value
-      listener(observable)
+      if (value !== obj[prop]) {
+        const prev = obj[prop]
+        obj[prop] = value
+        observer({ prop, prev, curr: value })
+      }
       return true
     }
   })
--- 08-structural-design-patterns/06-proxy-reactive/index.js
+++ 08-structural-design-patterns/06-proxy-reactive/index.js
@@ -1,19 +1,31 @@
 import { createObservable } from './create-observable.js'
 
+function calculateTotal (invoice) { // ①
+  return invoice.subtotal -
+    invoice.discount +
+    invoice.vat
+}
+
 const invoice = {
-  partial: 100,
+  subtotal: 100,
   discount: 10,
   vat: 20
 }
-let total
+let total = calculateTotal(invoice)
+console.log(`Starting total: ${total}`)
 
-function calculateTotal () {
-  total = invoice.partial - invoice.discount + invoice.vat
-  console.log('NEW TOTAL:', total)
-}
+const obsInvoice = createObservable( // ②
+  invoice,
+  ({ prop, prev, curr }) => {
+    total = calculateTotal(invoice)
+    console.log(`TOTAL: ${total} (${prop} changed: ${prev} -> ${curr})`)
+  }
+)
 
-const observableInvoice = createObservable(invoice, calculateTotal)
+// ③
+obsInvoice.subtotal = 200 // TOTAL: 210
+obsInvoice.discount = 20 // TOTAL: 200
+obsInvoice.discount = 20 // no change: doesn't trigger the observer
+obsInvoice.vat = 30 // TOTAL: 210
 
-observableInvoice.partial = 200 // NEW TOTAL: 210
-observableInvoice.discount = 50 // NEW TOTAL: 170
-observableInvoice.vat = 30 // NEW TOTAL: 180
+console.log(`Final total: ${total}`)
