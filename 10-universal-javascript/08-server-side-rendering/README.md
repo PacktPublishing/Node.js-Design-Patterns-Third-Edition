f023e761390abba8b7ee2205bb665b52508315c7
Luciano <lucianomammino@gmail.com>
2020-04-04 19:58:38 +0100

    Added server side rendering example

---
+++ 10-universal-javascript/08-server-side-rendering/README.md
@@ -0,0 +1,22 @@
+# 08-server-side-rendering
+
+This example demostrates how to evolve our frontend only app and add server side rendering.
+
+## Dependencies
+
+Install all the necessary dependencies with:
+
+```bash
+npm install
+```
+
+## Run
+
+To run the example:
+
+```bash
+npm start
+```
+
+Then you point your browser to [http://localhost:3000](localhost:3000).
+
---
+++ 10-universal-javascript/08-server-side-rendering/src/server.js
@@ -0,0 +1,54 @@
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import react from 'react'
+import reactServer from 'react-dom/server.js'
+import fastify from 'fastify'
+import fastifyStatic from 'fastify-static'
+import { StaticRouter } from 'react-router-dom'
+import { App } from './frontend/App.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const h = react.createElement
+const server = fastify({ logger: true })
+
+const template = ({ content }) => `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>My library</title>
+  </head>
+  <body>
+    <div id="root">${content}</div>
+    <script type="text/javascript" src="/public/main.js"></script>
+  </body>
+</html>`
+
+server.register(fastifyStatic, {
+  root: resolve(__dirname, '..', 'public'),
+  prefix: '/public/'
+})
+
+server.get('*', async (req, reply) => {
+  const location = req.raw.originalUrl
+  const staticContext = {}
+  const serverApp = h(StaticRouter, { location, context: staticContext }, h(App))
+  const content = reactServer.renderToString(serverApp)
+  const html = template({ content })
+
+  let code = 200
+  if (staticContext.statusCode) {
+    code = staticContext.statusCode
+  }
+
+  reply.code(code).type('text/html').send(html)
+})
+
+const port = Number.parseInt(process.env.PORT) || 3000
+const address = process.env.ADDRESS || '127.0.0.1'
+
+server.listen(port, address, function (err, addr) {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+})
