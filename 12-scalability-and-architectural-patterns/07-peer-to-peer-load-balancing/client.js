b88740d9b16769979a2e28e4b290aa5f1eb80cf6
Luciano <lucianomammino@gmail.com>
2020-04-29 19:54:47 +0100

    Using get-stream to simplify code

--- 12-scalability-and-architectural-patterns/07-peer-to-peer-load-balancing/client.js
+++ 12-scalability-and-architectural-patterns/07-peer-to-peer-load-balancing/client.js
@@ -2,15 +2,7 @@ import { balancedRequest } from './balancedRequest.js'
 
 async function main () {
   for (let i = 0; i < 10; i++) {
-    const response = await balancedRequest({ method: 'GET', path: '/' })
-    const body = await new Promise((resolve, reject) => {
-      let responseBody = ''
-      response.on('data', chunk => {
-        responseBody += chunk.toString()
-      })
-      response.on('end', () => resolve(responseBody))
-      response.on('error', reject)
-    })
+    const body = await balancedRequest({ method: 'GET', path: '/' })
     console.log(`Request ${i} completed:`, body)
   }
 }
