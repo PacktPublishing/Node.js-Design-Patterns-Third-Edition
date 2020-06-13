6c9ddd9d7ac20e78284adf86b1069b6ac91903df
Luciano <lucianomammino@gmail.com>
2020-06-13 13:19:04 +0100

    Switching to stack calculator to show the pattern with internal state

--- 08-structural-design-patterns/01-proxy-composition/index.js
+++ 08-structural-design-patterns/01-proxy-composition/index.js
@@ -1,42 +1,74 @@
-class Calculator {
-  divide (dividend, divisor) {
+class StackCalculator {
+  constructor () {
+    this.stack = []
+  }
+
+  putValue (value) {
+    this.stack.push(value)
+  }
+
+  peekValue () {
+    return this.stack[this.stack.length - 1]
+  }
+
+  divide () {
+    const divisor = this.stack.pop()
+    const dividend = this.stack.pop()
     return dividend / divisor
   }
 
-  multiply (multiplier, multiplicand) {
+  multiply () {
+    const multiplicand = this.stack.pop()
+    const multiplier = this.stack.pop()
     return multiplier * multiplicand
   }
 }
 
-class SafeCalculator extends Calculator {
+class SafeCalculator {
   constructor (calculator) {
-    super()
     this.calculator = calculator
   }
 
   // proxied method
-  divide (dividend, divisor) {
+  divide () {
     // additional validation logic
+    const divisor = this.calculator.peekValue()
     if (divisor === 0) {
       throw Error('Division by 0')
     }
     // if valid delegates to the subject
-    return this.calculator.divide(dividend, divisor)
+    return this.calculator.divide()
   }
 
-  // delegated method
+  // delegated methods
+  putValue (...args) {
+    return this.calculator.putValue(...args)
+  }
+
+  peekValue () {
+    return this.calculator.peekValue()
+  }
+
   multiply (...args) {
     return this.calculator.multiply(...args)
   }
 }
 
-const calculator = new Calculator()
+const calculator = new StackCalculator()
 const safeCalculator = new SafeCalculator(calculator)
 
-console.log(safeCalculator instanceof Calculator) // true
-console.log(calculator.multiply(3, 2)) // 6
-console.log(safeCalculator.multiply(3, 2)) // 6
-console.log(calculator.divide(4, 2)) // 2
-console.log(safeCalculator.divide(4, 2)) // 2
-console.log(calculator.divide(2, 0)) // Infinity
-console.log(safeCalculator.divide(2, 0)) // Error('Division by 0')
+calculator.putValue(3)
+calculator.putValue(2)
+console.log(calculator.multiply()) // 3*2 = 6
+
+safeCalculator.putValue(3)
+safeCalculator.putValue(2)
+console.log(safeCalculator.multiply()) // 3*2 = 6
+
+calculator.putValue(4)
+calculator.putValue(0)
+console.log(calculator.divide()) // 4/0 = Infinity
+
+safeCalculator.putValue(4)
+safeCalculator.putValue(0)
+console.log(safeCalculator.divide()) // 4/0 -> Error('Division by 0')
