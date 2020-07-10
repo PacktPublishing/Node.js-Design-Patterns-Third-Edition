3884af976a6642b57e9feca31ea5457c58018402
Mario Casciaro <mariocasciaro@gmail.com>
2020-07-10 18:23:24 +0100

    Standardize function definition in object literals

--- 09-behavioral-design-patterns/14-command/statusUpdateService.js
+++ 09-behavioral-design-patterns/14-command/statusUpdateService.js
@@ -3,14 +3,14 @@ const statusUpdates = new Map()
 
 // The Target
 export const statusUpdateService = {
-  postUpdate: function (status) {
+  postUpdate (status) {
     const id = Math.floor(Math.random() * 1000000)
     statusUpdates.set(id, status)
     console.log(`Status posted: ${status}`)
     return id
   },
 
-  destroyUpdate: id => {
+  destroyUpdate (id) {
     statusUpdates.delete(id)
     console.log(`Status removed: ${id}`)
   }
