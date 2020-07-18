7d9f8d93b8bcbda017633b64f734e58dcb8b2e6c
Mario Casciaro <mariocasciaro@gmail.com>
2020-07-18 17:06:13 +0100

    Import builtin modules first

--- 11-advanced-recipes/08-cpu-bound/subsetSumThreads.js
+++ 11-advanced-recipes/08-cpu-bound/subsetSumThreads.js
@@ -1,7 +1,7 @@
 import { EventEmitter } from 'events'
-import { ThreadPool } from './threadPool.js'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
+import { ThreadPool } from './threadPool.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const workerFile = join(__dirname,
